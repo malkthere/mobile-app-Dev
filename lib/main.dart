1d7c057625535051b032bbe1196f221076dcbe7e
mazin <malkthere@gmail.com>
2023-10-28 21:08:51 +0300

    State Management class 005-02

--- lib/main.dart
+++ lib/main.dart
@@ -1,148 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/framework.dart';
-void main() {
-  runApp(const MyApp());
-}
+import 'package:helloworld02102023/product.dart';
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home:  MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Product Navigation demo home page'),
     );
   }
 }
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key? key,required this.title}) : super(key: key);
   final String title;
+  final items = Product.getProducts();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title:Text("Product Listing")),
-        body: ListView(
-          shrinkWrap: true, padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
-          children: <Widget> [
-
-            ProductBox(
-                name: "iPhone",
-                description: "iPhone is the stylist phone ever",
-                price: 1000,
-                image: "iphone.png"
-            ),
-            ProductBox(
-                name: "Pixel",
-                description: "Pixel is the most featureful phone ever",
-                price: 800,
-                image: "pixel.png"
-            ),
-            ProductBox(
-                name: "Laptop",
-                description: "Laptop is most productive development tool",
-                price: 2000,
-                image: "laptop.png"
-            ),
-            ProductBox(
-                name: "Tablet",
-                description: "Tablet is the most useful device ever for meeting",
-                price: 1500,
-                image: "tablet.png"
-            ),
-            ProductBox(
-                name: "Pendrive",
-                description: "Pendrive is useful storage medium",
-                price: 100,
-                image: "pendrive.png"
-            ),
-            ProductBox(
-                name: "Floppy Drive",
-                description: "Floppy drive is useful rescue storage medium",
-                price: 20,
-                image: "floppy.png"
-            ),
-          ],
+        appBar: AppBar(title: Text("Product Navigation")),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              child: ProductBox(item: items[index]),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductPage(item: items[index]),
+                  ),
+                );
+              },
+            );
+          },
         )
     );
   }
-
-
-
 }
 
-class ProductBox extends StatelessWidget {
-  ProductBox({Key? key, required this.name,required this.description,required this.price,required this.image})
-      : super(key: key);
-  final String name;
-  final String description;
-  final int price;
-  final String image;
-
-  get onPressed => null;
-// user defined function
-  void _showDialog(BuildContext context) {
-// flutter defined function
-    showDialog(
-      context: context, builder: (BuildContext context) {
-// return object of type Dialog
-      return AlertDialog(
-        title: new Text("Message"),
-        content: new Text(this.name +"is not available"),
-        actions: <Widget>[
-          new ElevatedButton(
-            child: new  Text("Close"),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
-    );
-  }
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(2), height: 155,  child: Card(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
-                Image.asset("assets/appimages/" +image),
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(5), child: Column(
-                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                        Text(this.name, style: TextStyle(fontWeight:FontWeight.bold)),
-                        Text(this.description),
-                        Text("Price: " + this.price.toString()),
-                       GestureDetector(
-                        onTap: () {
-                          _showDialog(context);
-                        },
-                        child: Text("buy"),
-                      )
-                    ],
-                  )
-                ),
-          ),
-        ]
-        )
-    )
-    );
-  }
-}
