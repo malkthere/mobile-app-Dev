bd2170ebefb19d6949e724df0436d1c5c9641698
mazin <malkthere@gmail.com>
2023-11-27 22:05:27 +0300

    class 006 Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,29 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class MyHomePage extends StatelessWidget {
+
+class MyHomePage extends StatefulWidget {
+  //const MyHomePage({super.key});
   MyHomePage({Key? key,required this.title}) : super(key: key);
   final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String type="Laptop";
   final items = Product.getProducts();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Product Navigation")),
-        body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
+      appBar: AppBar(title: Text("Product Navigation")),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          print("the item is "+items[index].name+"the index now is "+index.toString());
+          if(items[index].type==this.type) {
             return GestureDetector(
               child: ProductBox(item: items[index]),
               onTap: () {
@@ -39,9 +50,68 @@ class MyHomePage extends StatelessWidget {
                 );
               },
             );
-          },
-        )
+            //
+          }else{
+            return Text("");
+          }
+
+        },
+        //
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text('Laptops'),
+              onTap: () {
+                setState(() {
+                  type="Laptop";
+                });
+                Navigator.pop(context);
+
+              },
+            ),ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text('Books'),
+              onTap: () {
+                setState(() {
+                  type="Book";
+                });
+                Navigator.pop(context);
+
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Mobiles'),
+              onTap: () {
+                setState(() {
+                  type="Mobile";
+                });
+                Navigator.pop(context);
+
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
 
--- lib/product.dart
+++ lib/product.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 class Product {
   final String name;
+  final String type;
   final String description;
   final int price;
   final String image;
-  Product(this.name, this.description, this.price, this.image);
+  Product(this.name, this.type, this.description, this.price, this.image);
 
   static List<Product> getProducts() {
     List<Product> items = <Product>[];
     items.add(
         Product(
             "Pixel",
+            "Mobile",
             "Pixel is the most featureful phone ever",
             800,
             "pixel.png"
@@ -18,31 +20,88 @@ class Product {
     );
     items.add(
         Product(
+            "Toshiba laptop 1",
             "Laptop",
-            "Laptop is most productive development tool",
+            "Toshiba Laptop with core i7 processor",
             2000,
             "laptop.png"
         )
     );
     items.add(
         Product(
-            "Tablet",
-            "Tablet is the most useful device ever for meeting",
+            "Dell laptop D5500",
+            "Laptop",
+            "Dell Laptop with core i7 processor and 16 Gb RAM",
             1500,
             "tablet.png"
         )
     );
     items.add(
         Product(
-            "Pendrive",
-            "iPhone is the stylist phone ever",
+            "Rich Dad Poor Dad",
+            "Book",
+            "is a 1997 book written by Robert T. Kiyosaki and Sharon Lechter. It advocates the importance of financial literacy, financial independence and building wealth through investing in assets, real estate investing, starting and owning businesses, as well as increasing one's financial intelligence.",
             100,
             "pendrive.png"
         )
     );
     items.add(
         Product(
-            "Floppy Drive",
+            "HP laptop ZBOOK",
+            "Laptop",
+            "high quality ZBook Laptop with core i7 processor and 16 Gb RAM",
+            20,
+            "floppy.png"
+        )
+    );
+    items.add(
+        Product(
+            "Redmi note 11",
+            "Mobile",
+            "Redmi is the stylist note phone ever with high capabilities",
+            1000,
+            "iphone.png"
+        )
+    );items.add(
+        Product(
+            "Redmi  11",
+            "Mobile",
+            "Redmi is the latest phone  with high capabilities",
+            800,
+            "pixel.png"
+        )
+    );
+    items.add(
+        Product(
+            "Toshiba laptop 2",
+            "Laptop",
+            "Toshiba Laptop with core i5 processor",
+            2000,
+            "laptop.png"
+        )
+    );
+    items.add(
+        Product(
+            "Dell laptop D5500",
+            "Laptop",
+            "Dell Laptop with core i5 processor and 8 Gb RAM",
+            1500,
+            "tablet.png"
+        )
+    );
+    items.add(
+        Product(
+            "Men Are from Mars, Women Are from Venus ",
+            "Book",
+            "(1992) is a book written by American author and relationship counselor John Gray. The book states that most common ...",
+            100,
+            "pendrive.png"
+        )
+    );
+    items.add(
+        Product(
+            "HP laptop ZBOOK",
+            "Laptop",
             "iPhone is the stylist phone ever",
             20,
             "floppy.png"
@@ -50,7 +109,8 @@ class Product {
     );
     items.add(
         Product(
-            "iPhone",
+            "Redmi note 11",
+            "Mobile",
             "iPhone is the stylist phone ever",
             1000,
             "iphone.png"
@@ -207,7 +267,7 @@ class ProductBox extends StatelessWidget {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Text(this.item.name, style: TextStyle(fontWeight: FontWeight.bold)), Text(this.item.description),
+                            Text(this.item.name, style: TextStyle(fontWeight: FontWeight.bold)), Text(this.item.description, overflow: TextOverflow.ellipsis,),
                             Text("Price: " + this.item.price.toString()),
                             RatingBox(),
                           ],
